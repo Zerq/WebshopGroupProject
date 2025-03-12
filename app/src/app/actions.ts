0cf557d3e12fb9fde8b1ffcbc2b2fbb537165674
Cat <Zerq_TM@hotmail.com>
2025-03-12 14:38:56 +0100

    added typing for product fetch

---
+++ app/src/app/actions.ts
@@ -0,0 +1,52 @@
+
+export const fetchProducts = async () => {
+    const res = await fetch('https://dummyjson.com/products')
+    const data = await res.json() as Product[];
+    return data;
+}
+
+export interface Dimensions {
+    width: number;
+    height: number;
+    depth: number;
+}
+
+export interface Meta {
+    createdAt: string,
+    updatedAt: string,
+    barcode: string,
+    qrCode: string
+}
+
+export interface Rating {
+    rating: number;
+    comment: string;
+    date: string;
+    reviewerName: string;
+    reviewerEmail: string;
+}
+
+export interface Product {
+    id: number,
+    title: string,
+    description: string,
+    category: string,
+    price: number,
+    discountPercentage: number,
+    rating: number,
+    stock: number,
+    tags: string[],
+    brand: string,
+    sku: string,
+    weight: number,
+    dimensions: Dimensions,
+    warrantyInformation: string,
+    shippingInformation: string,
+    availabilityStatus: string,
+    reviews: Rating[]
+    returnPolicy: string,
+    minimumOrderQuantity: number,
+    meta: Meta,
+    images: string[], 
+    thumbnail: string
+}
