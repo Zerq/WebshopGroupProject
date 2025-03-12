c1755ef3c3308bb527e195e256a6ef5a9df47541
Lucian Ralea <lucian.ralea@gmail.com>
2025-03-12 16:34:23 +0100

    Removed console.log() after fetching products

--- app/src/app/actions.ts
+++ app/src/app/actions.ts
@@ -2,6 +2,5 @@
 export const fetchProducts = async () => {
     const res = await fetch('https://dummyjson.com/products')
     const data = await res.json();
-    console.log(data);
     return data;
 }
