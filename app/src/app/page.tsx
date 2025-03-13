a751f237474b54bdfcebd494e0204af5efcf92a1
Cat <Zerq_TM@hotmail.com>
2025-03-13 19:03:35 +0100

    made a factory for fetching that data and added it to the provider
    made a factory for fetching that data and added it to the provider

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,14 +1,26 @@
-import Image from "next/image";
-import { fetchProducts } from "./actions";
+"use client";
+import { useContext, useEffect } from "react";
 import { ProductList } from "./components/product-cards/products";
+import { GeneralContext } from "./generalprovider";
 
-export default async function Home() {
-  const {products} =  await fetchProducts();
+export default function Home() {
+
   
+  const Context = useContext(GeneralContext);
+  if (!Context) throw new Error('GeneralContext måste användas inom en GeneralProvider');
+  const { state, getProducts } = Context;
+
+  useEffect(()=> {
+    (async ()=> {
+      await getProducts(n=> n.limit(20).skip(40));
+    })()
+  })  
+
+
   return (
     <div>
       <main>
-        <ProductList products={products}/>
+        <ProductList products={state?.products ?? []}/>
       </main>
     </div>
   );
