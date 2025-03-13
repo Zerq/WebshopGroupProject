af102c84a7e758ffad801686e8837844be5bc30f
Cat <Zerq_TM@hotmail.com>
2025-03-13 16:13:07 +0100

    Create orderby.tsx
    ..

---
+++ app/src/app/components/order-by/orderby.tsx
@@ -0,0 +1,18 @@
+import { SyntheticEvent } from "react";
+
+export default function OrderBy(){
+
+    const select = (e:SyntheticEvent<HTMLSelectElement, Event>)=> {
+
+    };
+
+    return <div>
+        <label htmlFor="OrderBySelect">OrderBy</label>
+        <select onSelect={select} id="OrderBySelect" name="OrderBySelect">
+            <option value={undefined}>None</option>
+            <option value="Price">None</option>
+            <option value="Category">None</option>
+            <option value="Rating">None</option>          
+        </select>
+    </div>
+}
