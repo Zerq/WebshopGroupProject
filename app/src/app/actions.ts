10985de175d744ee76c70e8bee096aabafd04a6b
Cat <Zerq_TM@hotmail.com>
2025-03-14 01:05:58 +0100

    cleanup

--- app/src/app/actions.ts
+++ app/src/app/actions.ts
@@ -1,10 +1,10 @@
-import { Condiment, Rethink_Sans, Thasadith } from "next/font/google";
 import { Product } from "./types";
 
 export const fetchProducts = async () => {
     const res = await fetch('https://dummyjson.com/products');
     return await res.json() as Product[];
 }
+
 
 
 
