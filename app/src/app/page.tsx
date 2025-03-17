6a3d66357b914d604116e389b29fb3a08abe5674
Tony Berglund <tony_berglund@outlook.com>
2025-03-17 11:07:29 +0100

    Fixed paginering without infloop

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -8,30 +8,29 @@ import PaginationNav from "./components/navigation/pagination-nav";
 
 export default function Home() {
   const [state, setState] = useState({ products: [], total: 0 } as ProductResult);
-  let query = Products.GetProducts();
 
-  const queryParameters = new URLSearchParams(window.location.search)
-
-  
-  if (queryParameters.has("limit")) {
-    const limit = queryParameters.get("limit");
-    if (!limit) throw new Error("Limit is null");
-    const numberLimit = Number.parseInt(limit);
-    if (Number.isNaN(numberLimit)) Error("Limit is not a number");
-    query = query.limit(numberLimit);
-  }
-
-  if (queryParameters.has("skip")) {
-    const skip = queryParameters.get("skip");
-    if (!skip) throw new Error("Skip is null");
-    const numberSkip = Number.parseInt(skip);
-    if (Number.isNaN(numberSkip)) Error("Skip is not a number");
-    query = query.skip(numberSkip);
-  }
 
   useEffect(() => {
+    let query = Products.GetProducts();
+    const queryParameters = new URLSearchParams(window.location.search)
+    if (queryParameters.has("limit")) {
+      const limit = queryParameters.get("limit");
+      if (!limit) throw new Error("Limit is null");
+      const numberLimit = Number.parseInt(limit);
+      if (Number.isNaN(numberLimit)) Error("Limit is not a number");
+      query = query.limit(numberLimit);
+    }
+
+    if (queryParameters.has("skip")) {
+      const skip = queryParameters.get("skip");
+      if (!skip) throw new Error("Skip is null");
+      const numberSkip = Number.parseInt(skip);
+      if (Number.isNaN(numberSkip)) Error("Skip is not a number");
+      query = query.skip(numberSkip);
+    }
     query.fetch().then(n => setState(n));
-  }, [query]);
+  }, []);
+  
 const limit = 25;
   const pageCount = Math.round(state.total/limit);
   return (
