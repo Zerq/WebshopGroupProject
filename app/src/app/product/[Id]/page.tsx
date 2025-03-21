3dcbf63e5e92715f502e992515ef712196d0bca6
Lucian Ralea <lucian.ralea@gmail.com>
2025-03-21 05:26:51 +0100

    Koden som ska hämta data om en specifik produkt behöver fixas.

--- app/src/app/product/[Id]/page.tsx
+++ app/src/app/product/[Id]/page.tsx
@@ -1,10 +1,31 @@
-import { fetchProduct, Products } from "@/app/actions";
+"use client"
 
-let productData = fetchProduct();
-console.log(productData);
+import { fetchProducts, Products } from "@/app/actions";
+import { ProductResult } from "@/app/types";
+import { useEffect, useState } from "react";
 
-export default function ProductDetail(){
-    return(
+
+export default function ProductDetail() {
+    const [product, setProduct] = useState({ products: [], total: 0 } as ProductResult);
+    const [error, setError] = useState<string | null>(null);
+
+    // useEffect(() => {
+    //     console.log("Fetching products data...");
+    //     const loadData = async () => {
+    //         try {
+    //             const data = Products.GetProducts();
+    //             setProduct(data);
+    //         }
+    //         catch (err) {
+    //             setError("Kunde inte ladda produkterna. Försök igen senare");
+    //             console.error("Fetch error:", err);
+    //         }
+    //     };
+    //         loadData();
+
+    //     }, []);
+
+    return (
         <div>
             <h2>Produktdetaljer för ...</h2>
         </div>
