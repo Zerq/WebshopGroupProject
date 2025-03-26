c9511994f74cd0df26a2aa0eb3361ca6ce05e930
Lucian Ralea <lucian.ralea@gmail.com>
2025-03-26 22:04:38 +0100

    Kampanjprodukterna visas nu. Designen är någorlunda responsiv.

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -5,14 +5,15 @@ import { ProductList } from "./components/product-cards/products";
 import { getCampaignIds } from "./actions";
 import { ProductResult } from "./types";
 import styles from "./page.module.css";
+import Image from "next/image";
 
 
 export default function Home() {
-  const productIds:Array<string> = ["1","2","3"];
-  const [state, setState] = useState({ products:[], total:0} as ProductResult);
-  const [isDoneLoading, setIsDoneLoading]= useState(false);
+  const productIds: Array<string> = ["1", "2", "3"];
+  const [state, setState] = useState({ products: [], total: 0 } as ProductResult);
+  const [isDoneLoading, setIsDoneLoading] = useState(false);
   useEffect(() => {
-    getCampaignIds(productIds).then(n=> {
+    getCampaignIds(productIds).then(n => {
       setState(n);
       setIsDoneLoading(true);
     });
@@ -20,12 +21,32 @@ export default function Home() {
 
 
   return !isDoneLoading ? <div className={styles.loadScreen}></div> :
-  <div className={styles.allWrapper}>
-        <div className={styles.categoryWrapper}>
-          Kategori ikoner
+    <div className={styles.allWrapper}>
+      <div className={styles.categoryWrapper}>
+        Kategori ikoner
+      </div>
+      <div className={styles.campaignWrapper}>
+        <div className={styles.imageWrapper}>
+          <Image
+            className={styles.image}
+            src="/pask-02.png"
+            width={350}
+            height={350}
+            alt={`Image of Eastern eggs in a basket`}
+          />
         </div>
-        <div className={styles.campaignWrapper}>
-        <ProductList products={state.products ?? []} />
+        <div className={styles.chosenProducts}>
+          <ProductList products={state.products ?? []} />
         </div>
+        {/* <div className={styles.campaignImg}>
+          <Image
+            className={styles.image}
+            src="/pask-02.png"
+            width={400}
+            height={400}
+            alt={`Image of Eastern eggs in a basket`}
+          />
+        </div> */}
+      </div>
     </div>
 }
