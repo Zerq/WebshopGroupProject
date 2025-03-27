726b6fad627280b9e7b444680b27c3b1c67eccae
Cat <Zerq_TM@hotmail.com>
2025-03-27 13:26:48 +0100

    fixed the size issue
    fixed the size issue

--- app/src/app/components/star/star.tsx
+++ app/src/app/components/star/star.tsx
@@ -1,10 +1,10 @@
 import style from "./star.module.css";
 export default function start(props: { score: number }) {
     const score = props.score;
-    const frac = score / 5;
+    const frac = ((score / 5) * 100) + "%";
 
     return <div className={style.stars} title={score + "/5"}>
-        <div className={style.frac} data-score={frac}>
+        <div className={style.frac} style={{width: frac}}>
             <p className={style.score}>{score}/5</p>
         </div>
     </div>;
