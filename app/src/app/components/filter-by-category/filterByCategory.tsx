f94856d291bb98c7a9b176e6c7b52320db99b192
Tony Berglund <tony_berglund@outlook.com>
2025-03-27 22:26:34 +0100

    Fixed dropdown styling.

--- app/src/app/components/filter-by-category/filterByCategory.tsx
+++ app/src/app/components/filter-by-category/filterByCategory.tsx
@@ -40,7 +40,7 @@ export default function FilterByCategory() {
     return <div className={styles.OrderBy}>
         <label className={styles.labelSelector} htmlFor="FilterBySelect"></label>
         <select onChange={select} defaultValue={params.get("orderBy") ?? "none"} id="FilterBySelect" className={styles.filterBySelect} name="FilterBySelect">
-            {...[<option key="-1" value="none" className="{kategorier}">-- Alla Kategorier --</option>, ...categories.map((n,i)=> <option  key={i} value={n}>{n}</option>)]}
+            {...[<option key="-1" value="none">- - - Alla Kategorier - - -</option>, ...categories.map((n, i) => <option className={styles.categoryOption} key={i} value={n}>{n}</option>)]}
 
         </select>
     </div>;
