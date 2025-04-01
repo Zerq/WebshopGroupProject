2c28e3e1535feee00350466fec28e578a16467f5
Tony Berglund <tony_berglund@outlook.com>
2025-04-01 21:02:03 +0200

    Merge with main

--- app/src/app/components/product-cards/campaignProducts.tsx
+++ app/src/app/components/product-cards/campaignProducts.tsx
@@ -4,6 +4,7 @@ import styles from "./cards.module.css";
 import { Product } from "../../types";
 import { useCart } from "@/app/cartprovider";
 import { useRouter } from "next/navigation";
+import { Star } from "lucide-react";
 
 
 export function CampaignProductList({ products }: { products: Product[] }) {
@@ -15,10 +16,15 @@ export function CampaignProductList({ products }: { products: Product[] }) {
         </ul>
     );
 }
+function DiscountedPrice(product: Product) {
+    const discountedPrice = Math.ceil(product.price * 0.75);
+    return discountedPrice;
+}
 
 export function Card({ product }: { product: Product }) {
     const { addToCart } = useCart();
     const router = useRouter();
+    const discountedPrice = DiscountedPrice(product);
    
     return (
         <li className={styles.card} aria-label={`Länk till ${product.title}`}>
@@ -39,14 +45,15 @@ export function Card({ product }: { product: Product }) {
                 </div>
                 <div className={styles.lowerHalf}>
                     <div>
-                        <div>
-                            <p className={styles.paraD}>Pris:</p>
-                            <span className={styles.price}>&euro;{Math.ceil(product.price * 0.75)}</span>
+                        <div className={styles.prices}>
+                            <span className={styles.oldPrice}>Förr: &euro;{product.price}</span>
+                            <span className={styles.discountedPrice}>Nu &euro;{discountedPrice}</span>
                         </div>
-                        <div>
-                            <p className={styles.paraD}>Kundbetyg:</p>
-                            {product.rating} av 5
-                        </div>
+                       
+                        <p className={styles.paraD}>Kundbetyg:&nbsp;&nbsp; {Array.from({ length: Math.round(product.rating) }, (_, i) => (
+                            <Star key={i} className={styles.star} />
+                        ))}
+                        </p>
                         <p className={styles.paraSmall}>{product.description}</p>
                     </div>
                     <div className={styles.btnWrapper}>
